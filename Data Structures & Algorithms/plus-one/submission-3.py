ddc64c792e1414b2df142c3504748cc02c3c36be
Akshay K <krisshvish@gmail.com>
2026-08-07 02:21:19 +0530

    Add: plus-one - submission-3

---
+++ Data Structures & Algorithms/plus-one/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        rev = digits[::-1]
+        carry = 0
+        i = 0
+        print(rev)
+        while i < len(rev):
+            if (rev[i] + 1) % 10 == 0:
+                carry = 1
+                rev[i] = 0
+            else:
+                rev[i] = rev[i] + 1
+                print(rev)
+                carry = 0
+                break
+            i = i+1
+
+        if carry > 0:
+            rev.append(1)
+        return rev[::-1]
